486912bda09e6490f90f9f8ac87d7e7532380583
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-07-29 15:56:43 +0000

    Setup GitHub User Search App structure with Axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Welcome! Start building your GitHub search app here.</p>
+    </div>
+  );
+}
+
+export default App;
