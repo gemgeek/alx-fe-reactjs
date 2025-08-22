75e0e2d9c29fba3812ca45087eab1b6826b05999
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-08-22 12:57:17 +0100

    Move tests to components folder

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../components/TodoList';
+import TodoList from './TodoList';
 
 test("renders todo items", () => {
   const todos = ["Learn React", "Write Tests"];
